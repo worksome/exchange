f061026cf931a614092a2c4247e5e630a008b5f6
Luke Downing <lukeraymonddowning@gmail.com>
2022-02-23 10:52:10 +0000

    Initial commit

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,6 +4,8 @@ namespace Worksome\Exchange\Tests;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Orchestra\Testbench\TestCase as Orchestra;
+use Symfony\Component\Console\Output\BufferedOutput;
+use Termwind\Termwind;
 use Worksome\Exchange\ExchangeServiceProvider;
 
 class TestCase extends Orchestra
@@ -11,6 +13,8 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
+
+        Termwind::renderUsing(new BufferedOutput());
 
         Factory::guessFactoryNamesUsing(
             fn (string $modelName) => 'Worksome\\Exchange\\Database\\Factories\\' . class_basename($modelName) . 'Factory'
