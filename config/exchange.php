a6ddd7abbdd528d36b795866bad5ec16a7dfb092
Saifallak <Saifallak@yahoo.com>
2024-03-22 15:08:28 +0200

    revert changes.

--- config/exchange.php
+++ config/exchange.php
@@ -11,7 +11,7 @@ return [
      * Supported: 'null', 'fixer', 'exchange_rate', 'frankfurter', 'cache'
      */
 
-    'default' => env('EXCHANGE_DRIVER', 'exchange_rate'),
+    'default' => env('EXCHANGE_DRIVER', 'frankfurter'),
 
     'services' => [
 
@@ -36,8 +36,8 @@ return [
         |--------------------------------------------------------------------------
         |
         | ExchangeRate is a paid service for converting currency codes. To use ExchangeRate, you'll
-        | need an API Access Key from the ExchangeRate dashboard. Set that here, and then
-        | change the 'default' to 'exchange_rate' or set EXCHANGE_DRIVER to 'exchange_rate'.
+        | need an API Access Key from the ExchangeRate dashboard. Set that here, and then change
+        | the 'default' to 'exchange_rate' or set EXCHANGE_DRIVER to 'exchange_rate'.
         |
         */
 
@@ -73,9 +73,10 @@ return [
         */
 
         'cache' => [
-            'strategy' => 'exchange_rate',
-            'ttl' => 60 * 60 * 24, // 24 hours
-            'key' => 'cached_exchange_rates',
+            'strategy' => env('EXCHANGE_RATES_CACHE_STRATEGY', 'frankfurter'),
+            'ttl' => env('EXCHANGE_RATES_CACHE_TTL', 60 * 60 * 24), // 24 hours
+            'key' => env('EXCHANGE_RATES_CACHE_KEY', 'cached_exchange_rates'),
+            'store' => env('EXCHANGE_RATES_CACHE_STORE'),
         ],
     ],
 
